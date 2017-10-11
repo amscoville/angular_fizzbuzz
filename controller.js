e95f95d9773510aa9a15c48b458bf0af0795bc02
Amy Scoville <amyscoville@Amys-MacBook-Air.local>
2017-10-11 16:02:53 -0600

    First Commit

---
+++ controller.js
@@ -0,0 +1,21 @@
+(function(){
+    var app = angular.module('controller', []);
+
+    app.controller('fizzBuzz', function(){
+        this.numbers = function (){
+            const arr = [];
+            for(let i = 1; i < 101; i++) {
+                if(i % 3 === 0 && i % 5 === 0) {
+                    arr.push('FIZZBUZZ');
+                } else if (i % 3 === 0) {
+                    arr.push('FIZZ');
+                } else if (i % 5 === 0) {
+                    arr.push('BUZZ');
+                } else {
+                    arr.push(i);
+                }  
+            }
+            return arr;
+        }; 
+    });
+})();
